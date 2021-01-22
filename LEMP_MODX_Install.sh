da13d93e059907629b0e6a0ee1314e43e2c9c885
Vague Rabbit <ethanvandal@gmail.com>
2021-01-22 10:44:45 -0800

    hopefully changed EOL to Unix

--- LEMP_MODX_Install.sh
+++ LEMP_MODX_Install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Author: Aaron K. Nall
+set -e
 if [[ ! $(sudo echo 0) ]]; then exit; fi
 echo ""
 echo "Welcome to the Ubuntu/Debian LEMP Stack Installer."
@@ -19,6 +20,7 @@ confirmInstall(){
 }
 
 confirmInstall
+
 while [[ "$validConfirm" = "" ]]
 do 
         case "$confirm" in
@@ -27,12 +29,10 @@ do
                         echo "You will be asked for input, please be patient."
                         sleep 5
                         ;;
-
                 n | N)
                         echo "Exiting Ubuntu/Debian LEMP Stack Installer."
                         exit
                         ;;
-
                 *)
                         confirm=""
                         echo "Invalid input detected."
@@ -47,13 +47,11 @@ sudo usermod -aG www-data $UN
 wait
 sudo usermod -g www-data $UN
 wait
-
 # Update apt
 sudo apt-get update
 wait
 sudo apt-get upgrade -y
 wait
-
 # Instal firewall & security basics
 sudo apt-get install -y fail2ban ufw
 wait
@@ -64,8 +62,8 @@ do
         echo "  Reference https://en.wikipedia.org/wiki/List_of_tz_database_time_zones):"
         read selectedTimezone
 done
-
 echo "Selected Timezone: $selectedTimezone"
+sleep 5
 
 #Install base packages
 sudo apt-get install -y build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils
@@ -76,7 +74,6 @@ wait
 #Set the timezone to $selectedTimezone
 sudo ln -sf /usr/share/zoneinfo/$selectedTimezone /etc/localtime
 wait
-
 sudo apt install nginx -y
 wait
 sudo systemctl enable nginx
@@ -86,7 +83,6 @@ wait
 
 #Install PHP7.4 and common PHP packages
 echo "Installing PHP..."
-
 sudo apt install -y php-fpm php-mysql php-json php-mbstring
 wait
 sudo systemctl enable php7.4-fpm
@@ -133,7 +129,6 @@ sudo sed -i "s/;date.timezone.*/date.timezone = $selectedTimezone/" /etc/php/7.4
 wait
 
 #Tune PHP-FPM pool settings
-
 sudo sed -i "s/;listen\.mode =.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
 wait
 sudo sed -i "s/;request_terminate_timeout =.*/request_terminate_timeout = 60/" /etc/php/7.4/fpm/pool.d/www.conf
@@ -150,7 +145,6 @@ sudo sed -i "s/;pm\.max_requests =.*/pm.max_requests = 500/" /etc/php/7.4/fpm/po
 wait
 
 #Tweak Nginx settings
-
 sudo sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
 wait
 sudo sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
@@ -161,7 +155,6 @@ sudo sed -i "s/# server_tokens off/server_tokens off/" /etc/nginx/nginx.conf
 wait
 
 #Configure Gzip for Nginx
-
 sudo cat > /etc/nginx/conf.d/gzip.conf << EOF
 gzip_comp_level 5;
 gzip_min_length 256;
@@ -190,7 +183,6 @@ wait
 #apt-get install -y nodejs
 
 #Install MySQL and set a strong root password
-
 echo "Installing MySQL Server..."
 sudo apt install -y mysql-server
 wait
@@ -207,7 +199,6 @@ PMA_DB_USER="pma_$DB_OBF2"
 PMA_DB_PASS=$(cat /dev/urandom | tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' | fold -w 128 | head -n 1)
 DB_ADMIN="dba_$DB_OBF3"
 DB_ADMIN_PASS=$(cat /dev/urandom | tr -dc 'A-Za-z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~' | fold -w 128 | head -n 1)
-
 sudo mysql -uroot <<MYSQL_SCRIPT
 CREATE DATABASE $MODX_DB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER '$MODX_DB_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MODX_DB_PASSWORD';
@@ -219,6 +210,7 @@ GRANT ALL ON *.* TO '$DB_ADMIN'@'localhost' WITH GRANT OPTION;
 MYSQL_SCRIPT
 wait
 
+# Make sure the current directory is home
 cd /home/$UN
 # The phpMyAdmin available in the Ubuntu OS repository for Ubuntu 20.04 may be old. So, we will download the latest version of phpMyAdmin from the official website.
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.tar.gz
@@ -230,14 +222,12 @@ wait
 
 # Create blowfish key for config file
 BLOWFISH=$(cat /dev/urandom | tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' | fold -w 32 | head -n 1)
-
 sudo sed -i "s/{blowfish}/$BLOWFISH/" /home/$UN/LEMP_MODX_Install_Bash/config.inc.php
 wait
 sudo sed -i "s/{controluser}/$PMA_DB_USER/" /home/$UN/LEMP_MODX_Install_Bash/config.inc.php
 wait
 sudo sed -i "s/{controlpass}/$PMA_DB_PASS/" /home/$UN/LEMP_MODX_Install_Bash/config.inc.php
 wait
-
 sudo rm /usr/share/phpMyAdmin/config.inc.php
 wait
 sudo cp /home/$UN/LEMP_MODX_Install_Bash/config.inc.php /usr/share/phpMyAdmin/
@@ -268,6 +258,7 @@ sitesAvailable='/etc/nginx/sites-available'
 webRoot='/var/www'
 domainRegex="^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$"
 
+# Ask the user for the domain
 acquireDomain(){
         while [ "$OWNER_IP" = "" ]
         do
@@ -281,17 +272,14 @@ acquireDomain(){
                 read domain
         done
         echo ""
-
         until [[ $domain =~ $domainRegex ]]
         do
                 echo -n "Enter a valid domain:"
                 read domain
         done
         echo ""
-
         echo -n "Enter the sub domain (optional):"
                 read subdomain
-
         if [ -z "$subdomain" ]
         then
                 newdomain="$domain"
@@ -300,7 +288,6 @@ acquireDomain(){
         fi
         echo ""
 }
-
 acquireDomain
 
 # SSH, HTTP and HTTPS
@@ -328,22 +315,22 @@ wait
 echo "$UFWS"
 sleep 5
 
+# Check the entered domain for existance
 while [ -e $newdomain ]
 do
         echo "This domain already exists, please choose a different domain."
         acquireDomain
 done
-
 echo "Using $newdomain for this LEMP installation"
 sleep 5
 
+# Check directories for existance
 if ! [ -d $sitesEnabled ]; then
         sudo mkdir -pv $sitesEnabled
         wait
         sudo chmod 777 $sitesEnabled
         wait
 fi
-
 if ! [ -d $sitesAvailable ]; then
         sudo mkdir -pv $sitesAvailable
         wait
@@ -385,12 +372,16 @@ wait
 sudo apt install -y python3-certbot-nginx
 wait
 
+# Keep the user informed
 echo "The LEMP Stack has been Installed"
 echo ""
 sleep 5
 echo "Now downloding latest MODX..."
 
+# Make sure the current directory is home
 cd /home/$UN
+
+# Download MODX
 wget -O modx.zip https://modx.com/download/direct?id=modx-2.8.1-pl-advanced.zip&0=abs
 wait
 unzip modx.zip
@@ -402,7 +393,6 @@ wait
 sudo mv /home/$UN/modx-2.8.1-pl /home/$UN/modx
 wait
 MODXCOREPATH="/home/$UN/modx/core"
-
 sudo chown -R www-data:www-data /home/$UN/modx/core
 wait
 sudo mkdir /usr/share/phpMyAdmin/tmp
@@ -424,22 +414,20 @@ wait
 
 #Setup logrotate for our Nginx logs
 #Execute the following to create log rotation config for Nginx - this gives you 10 days of logs, rotated daily
-
 sudo cat > /etc/logrotate.d/vhost << EOF
 /var/www/logs/*.log {
- rotate 10
+         rotate 10
  daily
  compress
  delaycompress
  sharedscripts
- 
+
  postrotate
  systemctl reload nginx > /dev/null
  endscript
 }
 EOF
 wait
-
 sudo chown -R www-data:www-data /var/log/nginx/*
 wait
 sudo chown -R www-data:www-data /var/www
@@ -454,21 +442,26 @@ APT::Periodic::Unattended-Upgrade "1";
 EOF
 wait
 
+# Keep the user informed
 echo "MODX 2.8.1 download complete"
 echo ""
 sleep 5
 echo "Creating database for $newdomain..."
 sleep 2
 
+# Write the setup information to files
 FP="/home/$UN/mysql_info_$newdomain.txt"
 sudo echo "MySQL root Password: $MYSQL_ROOT_PASSWORD" > $FP
 FP2="/home/$UN/modx_db_info_$newdomain.txt"
+wait
 sudo echo "$newdomain Info
 ------------------------
 MODX DB Name: $MODX_DB
 MODX DB Username: $MODX_DB_USER
 MODX DB Password: $MODX_DB_PASSWORD" > $FP2
+wait
 
+# Display the setup information to the user
 echo "Open your browser and navigate to $newdomain/setup/"
 sleep 1
 echo ""

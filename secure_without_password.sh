bdf6b555c02432e261a3ce544f3a81be1fb30828
Vague Rabbit <ethanvandal@gmail.com>
2021-01-22 11:55:38 -0800

    added options in secure_without_password.sh

--- secure_without_password.sh
+++ secure_without_password.sh
@@ -2,31 +2,93 @@
 # Author: Aaron K. Nall
 set -e
 
+# Get the username of the actual user
+$UN="$(who am i | awk '{print $1}')"
+
+while [ -n "$1" ]; do # while loop starts
+	case "$1" in
+                -u)
+                        USERNAME="$2"
+                        shift
+                        ;;
+
+                -k)
+                        KEY="$2"
+                        shift
+                        ;;
+
+                -f
+                        FILE="true"
+                        shift
+                        ;;
+
+                -fn
+                        FILENAME="$2"
+                        shift
+                        ;;
+
+                *)
+                        echo "Option $1 is invalid."
+                        echo "Exiting"
+                        exit 1
+                        ;;
+	esac
+	shift
+done
+
 secure_user(){
-    while [[ "$KEY" = "" ]]
-    do
-        echo -n "Paste the SSH Private Key for $USER:"
-        read -s KEY
-    done
-
-    mkdir -p ~/.ssh
-    sudo echo "$KEY">| ~/.ssh/authorized_keys
-
-    echo "Private key was added to ~./ssh/authorized_keys"
-    echo ""
+    # Set the home directory in a variable
+    if [[ "$UN" != "root" ]] || [[ "$USERNAME" != "" ]]; then
+        if [[ "$USERNAME" != "" ]]; then
+            HD="/home/$USERNAME"
+        else
+            HD="/home/$UN"
+        fi
+    else
+        HD="/root"
+    fi
+    sudo mkdir -p $HD/.ssh
+    if [[ "$FILE" = "" ]] && [[ "$FILENAME" = "" ]]; then
+        while [[ "$KEY" = "" ]]
+        do
+            if [[ "$USERNAME" = "" ]]; then
+                echo -n "Paste the SSH Key for $UN:"
+            else
+                echo -n "Paste the SSH Key for $USERNAME:"
+            fi
+            read -s KEY
+        done
+        sudo echo "$KEY" >| $HD/.ssh/authorized_keys
+        echo "Private key was added to $HD/.ssh/authorized_keys"
+        echo ""
+    else
+        if [[ "$FILENAME" != "" ]]; then
+            sudo  mv $HD/LEMP_MODX_Install_Bash/$FILENAME $HD/.ssh/authorized_keys
+            echo "$HD/LEMP_MODX_Install_Bash/$FILENAME was copied to $HD/.ssh/authorized_keys"
+            echo ""
+        else
+            sudo  mv $HD/LEMP_MODX_Install_Bash/authorized_keys $HD/.ssh/authorized_keys
+            echo "$HD/LEMP_MODX_Install_Bash/authorized_keys was copied to $HD/.ssh/authorized_keys"
+            echo ""
+        fi
+    fi
 }
 
 secure_ssh(){
     sudo rm /etc/ssh/sshd_config
-    sudo cp ~/LEMP_MODX_Install_Bash/sshd_conf /etc/ssh/sshd_config
+    sudo cp $HD/LEMP_MODX_Install_Bash/sshd_conf /etc/ssh/sshd_config
 
-    echo "/etc/ssh/sshd_config was changed to reflect ~/LEMP_MODX_Install_Bash/sshd_conf"
+    echo "/etc/ssh/sshd_config was changed to reflect $HD/LEMP_MODX_Install_Bash/sshd_conf"
     echo ""
     sleep 1
 }
 
-if [[ "$USER" != "root" ]]; then
-    if [[ ! $(sudo echo 0) ]]; then exit; fi
+if [[ "$UN" != "root" ]]; then
+    if [[ ! $(sudo echo 0) ]]; then
+        echo "Permission Denied!"
+        echo ""
+        exit;
+    fi
     secure_ssh
     secure_user
     sudo service ssh reload

f34691635dfa6df29c39d63332dbba6a5ddffb61
Vague Rabbit <ethanvandal@gmail.com>
2021-01-22 17:02:49 -0800

    updated validate_password_policy to validate_password.policy

--- LEMP_MODX_Install.sh
+++ LEMP_MODX_Install.sh
@@ -277,7 +277,7 @@ DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 INSTALL COMPONENT 'file://component_validate_password';
-SET GLOBAL validate_password_policy=STRONG;
+SET GLOBAL validate_password.policy=STRONG;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 wait
